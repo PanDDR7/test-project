6096a67125d922d76297ea8c72fe5816f4a201d9
George <georgekuo2020@gmail.com>
2019-05-16 17:38:39 +0800

    #新增登入

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -3,6 +3,7 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import models.User;
 import play.libs.Json;
 import play.mvc.*;
 
@@ -21,7 +22,7 @@ public class HomeController extends Controller {
     public Result index() {
         return ok(views.html.index.render());
     }
-
+    /*
     public Result test(Http.Request request) {
         JsonNode parameter = request.body().asJson();
         if (!parameter.has("request_name")) {
@@ -30,5 +31,23 @@ public class HomeController extends Controller {
         ObjectNode response = new ObjectNode(JsonNodeFactory.instance);
         response.put("response_name", parameter.get("request_name").asText());
         return ok(response);
+    }
+     */
+    public Result test(Http.Request request){
+        JsonNode parameter = request.body().asJson();
+        User user = new User();
+        if(!parameter.has("account")){
+            return ok(Json.newObject().put("error_code", "00001"));
+        }
+        ObjectNode response = new ObjectNode(JsonNodeFactory.instance);
+        if(parameter.get("account").asText().equals(user.getAccount())){
+            if(parameter.get("pwd").asText().equals(user.getPwd())){
+                response.put("user_name",user.getUsername());
+                return ok(response);
+            }else {
+                return ok("login fail ! password error");
+            }
+        }
+        return ok("login fail ! account error");
     }
 }
---
+++ app/models/User.java
@@ -0,0 +1,19 @@
+package models;
+
+public class User {
+    private String account = "ABC";
+    private String username = "George";
+    private String pwd = "123";
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+}
